3a6f403c7c0748f96bfe69171e1e7914ba483f10
= <=>
2018-12-02 20:27:05 -0500

    asdf

---
+++ Assets/Explode.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explode : MonoBehaviour {
+    int radius = 10;
+    int power = 1000;
+
+    // Use this for initialization
+    void Start()
+    {
+        Vector3 explosionPos = transform.position;
+        explosionPos.x = explosionPos.x + 5;
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+
+            if (rb != null)
+                rb.AddExplosionForce(power, explosionPos, radius, 3.0F);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
