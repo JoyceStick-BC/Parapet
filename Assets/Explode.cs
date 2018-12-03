26f6721c3eb16db76e2e56b30ec8ce4eebc55ab8
= <=>
2018-12-03 15:51:11 -0500

    Updated Explosion

--- Assets/Explode.cs
+++ Assets/Explode.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class Explode : MonoBehaviour {
     int radius = 10;
-    int power = 1000;
+    int power = 400;
 
     // Use this for initialization
     void Start()
     {
         Vector3 explosionPos = transform.position;
+
         explosionPos.x = explosionPos.x + 5;
         Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
         foreach (Collider hit in colliders)
