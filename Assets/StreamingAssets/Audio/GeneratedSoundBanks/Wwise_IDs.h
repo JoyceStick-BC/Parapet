0d6d191356031dbafd71f0c94be491058015d8dc
unknown <maxime.gautier.w@gmail.com>
2019-02-22 23:46:51 -0500

    Added Ambiance and seagul

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,61 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PARAPET_SFX_AMBIANCE = 74477125U;
+        static const AkUniqueID PARAPET_SFX_SEAGUL = 2487091518U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace SEAGUL
+        {
+            static const AkUniqueID GROUP = 3014379214U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID SEAGUL_BURST = 2907836809U;
+                static const AkUniqueID SEAGUL_SINGLE = 1600352189U;
+            } // namespace SWITCH
+        } // namespace SEAGUL
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID SEAGUL = 3014379214U;
+        static const AkUniqueID SFX = 393239870U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID VERB = 3657263530U;
+    } // namespace AUX_BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
