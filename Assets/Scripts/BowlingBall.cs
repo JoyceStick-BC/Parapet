66bee804ce83f2e3a61fcd047f8ce17f22382503
rickyyang987 <35543674+rickyyang987@users.noreply.github.com>
2019-03-17 18:51:20 -0400

    Ball and skittles update

---
+++ Assets/Scripts/BowlingBall.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class BowlingBall : MonoBehaviour {
+
+    private Transform t;
+    private
+
+
+	// Use this for initialization
+	void Start () {
+        t = gameObject.transform;
+
+        if (GetComponent<VRTK_InteractableObject>() == null)
+        {
+            VRTK_Logger.Info("No Interactable Script");
+        }
+        GetComponent<VRTK_InteractableObject>().InteractableObjectGrabbed += new InteractableObjectEventHandler(BowlingBallGrab);
+
+    }
+
+    private void BowlingBallGrab(object sender, InteractableObjectEventArgs e)
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        gameObject.transform.position = t.position;
+	}
+}
