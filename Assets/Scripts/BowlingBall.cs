c842f0bd2d1747c1a7b7509845583026c64dbd88
rickyyang987 <35543674+rickyyang987@users.noreply.github.com>
2019-03-19 00:05:41 -0400

    Bowling ball spawning

--- Assets/Scripts/BowlingBall.cs
+++ Assets/Scripts/BowlingBall.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 using VRTK;
 
 public class BowlingBall : MonoBehaviour {
-
+    public GameObject spawn = GameObject.Find("Sphere (1)");
     private Transform t;
     private bool grabbed = false;
-
-
+    private float elapsed = 1;
+    private bool spawned = false;
 	// Use this for initialization
 	void Start () {
         t = gameObject.transform;
@@ -23,15 +23,33 @@ public class BowlingBall : MonoBehaviour {
 
     private void BowlingBallGrab(object sender, InteractableObjectEventArgs e)
     {
+        if (!grabbed)
+        {
+            elapsed = Time.time;
+        }
         grabbed = true;
-        Instantiate(gameObject);
+        gameObject.GetComponent<Rigidbody>().useGravity = true;
     }
 
     // Update is called once per frame
     void Update () {
-        if (!grabbed)
+        if (grabbed && !spawned)
         {
-            gameObject.transform.position = t.position;
+            VRTK_Logger.Info("Can spawn");
+            if (Time.time - elapsed > 3)
+            {
+                VRTK_Logger.Info("Spawned" + spawn.transform.position.x + " y: " + spawn.transform.position.y + " z: " + spawn.transform.position.z);
+                GameObject clone = Instantiate(spawn);
+                clone.GetComponent<VRTK_InteractableObject>().isGrabbable = true;
+                clone.GetComponent<Rigidbody>().useGravity = false;
+                clone.GetComponent<MeshRenderer>().enabled = true;
+                clone.GetComponent<SphereCollider>().enabled = true;
+                clone.GetComponent<BowlingBall>().spawn = spawn;
+                //clone.transform.position = t.position;
+                spawned = true;
+                //gameObject.GetComponent<MeshCollider>().isTrigger = false;
+            }
         }
+        
 	}
 }
