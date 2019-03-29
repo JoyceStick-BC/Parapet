f475b86ac3b259336971799ccc3796c06eefd35e
rickyyang <rickyyang987@gmail.com>
2019-03-29 11:42:01 -0400

    Added animation for jack in the box

---
+++ Assets/Scripts/JackInTheBox.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class JackInTheBox : MonoBehaviour {
+    public GameObject trigger;
+    public Animator myAnimationController;
+
+	// Use this for initialization
+	void Start () {
+        if (GetComponent<VRTK_InteractableObject>() == null)
+        {
+            Debug.Log("very sad");
+        }
+        GetComponent<VRTK_InteractableObject>().InteractableObjectTouched += new InteractableObjectEventHandler(TriggerAnimation);
+    }
+
+    private void TriggerAnimation(object sender, InteractableObjectEventArgs e)
+    {
+        //myAnimationController.SetBool("playanim", true);
+        myAnimationController.Play("Take 001");
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
