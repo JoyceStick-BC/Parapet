46bd25e5e09a5f783d756a7a2365084243c4785c
rickyyang987 <35543674+rickyyang987@users.noreply.github.com>
2019-03-31 11:11:59 -0400

    more balloon stuff

--- Assets/Scripts/ReleaseBalloons.cs
+++ Assets/Scripts/ReleaseBalloons.cs
@@ -9,9 +9,13 @@ public class ReleaseBalloons : MonoBehaviour {
     
     private Rigidbody[] balloons;
     private MeshRenderer[] renders;
+    private ConstantForce[] forces;
 
     private bool launched = false;
-    
+
+    private int totalBallons;
+    private int i = 0;
+
 	// Use this for initialization
 	void Start () {
         //balloons = balloonBasket1.
@@ -20,20 +24,41 @@ public class ReleaseBalloons : MonoBehaviour {
     }
     private void Release_Balloons(object sender, InteractableObjectEventArgs e)
     {
-        balloons = balloonBasket1.GetComponentsInChildren<Rigidbody>();
-        renders = balloonBasket1.GetComponentsInChildren<MeshRenderer>();
-        foreach (Rigidbody rb in balloons)
+        if (!launched)
         {
+            balloons = balloonBasket1.GetComponentsInChildren<Rigidbody>();
+            renders = balloonBasket1.GetComponentsInChildren<MeshRenderer>();
+            forces = balloonBasket1.GetComponentsInChildren<ConstantForce>();
 
+            foreach (Rigidbody rb in balloons)
+            {
+                rb.isKinematic = false;
+                rb.AddForce(Random.Range(-1, 1) * 2, 5, Random.Range(-1, 1) * 2, ForceMode.Impulse);
+            }
+            foreach (MeshRenderer mesh in renders)
+            {
+                //turn on meshes
+                Debug.Log("Turn on Balloon meshes");
+                mesh.enabled = true;
+            }
+            foreach (ConstantForce force in forces)
+            {
+                //apply constant force
+                force.enabled = true;
+            }
+            launched = true;
         }
-        foreach (MeshRenderer mesh in renders)
-        {
-
-        }
+        
     }
 
     // Update is called once per frame
     void Update () {
-		
+		//while (i < totalBallons)
+  //      {
+  //          balloons[i].isKinematic = false;
+  //          balloons[i].AddForce(Random.Range(-1, 1) * 2, 5, Random.Range(-1, 1) * 2, ForceMode.Impulse);
+  //          renders[i].enabled = true;
+  //          forces[i].enabled = true;
+  //      }
 	}
 }
