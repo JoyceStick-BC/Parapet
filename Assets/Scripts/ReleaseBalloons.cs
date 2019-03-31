f1eb3bfb091adaf1718489e63506352300e24da5
rickyyang <rickyyang987@gmail.com>
2019-03-31 19:49:29 -0400

    Balloon update

--- Assets/Scripts/ReleaseBalloons.cs
+++ Assets/Scripts/ReleaseBalloons.cs
@@ -15,68 +15,94 @@ public class ReleaseBalloons : MonoBehaviour {
 
     private bool launched = false;
 
-    private int totalBallons;
+    private int totalBalloons;
     private int i = 0;
 
-	// Use this for initialization
-	void Start () {
+    public Animator myAnimationController;
+    private bool animationPlayed = false;
+
+    private float countdown = 0.5f;
+    private float time;
+
+    // Use this for initialization
+    void Start () {
         //balloons = balloonBasket1.
         GetComponent<VRTK_InteractableObject>().InteractableObjectUsed += new InteractableObjectEventHandler(Release_Balloons);
 
     }
     private void Release_Balloons(object sender, InteractableObjectEventArgs e)
     {
+        if (!animationPlayed)
+        {
+            myAnimationController.Play("Take 001");
+            animationPlayed = true;
+        }
+
         if (!launched)
         {
-            
-            Debug.Log("Release");
-            balloons = balloonBasket1.GetComponentsInChildren<Rigidbody>();
-            renders = balloonBasket1.GetComponentsInChildren<MeshRenderer>();
-            forces = balloonBasket1.GetComponentsInChildren<ConstantForce>();
+            //Debug.Log("Release");
+            //balloons = balloonBasket1.GetComponentsInChildren<Rigidbody>();
+            //renders = balloonBasket1.GetComponentsInChildren<MeshRenderer>();
+            //forces = balloonBasket1.GetComponentsInChildren<ConstantForce>();
 
-            foreach (Rigidbody rb in balloons)
-            {
-                rb.isKinematic = false;
-                rb.AddForce(Random.Range(-1, 1) * 1, 4, Random.Range(-1, 1) * 1, ForceMode.Impulse);
-            }
-            foreach (MeshRenderer mesh in renders)
-            {
-                //turn on meshes
-                Debug.Log("Turn on Balloon meshes");
-                mesh.enabled = true;
-            }
-            foreach (ConstantForce force in forces)
-            {
-                //apply constant force
-                force.enabled = true;
-            }
+            //foreach (Rigidbody rb in balloons)
+            //{
+            //    rb.isKinematic = false;
+            //    rb.AddForce(Random.Range(-1, 1) * 1, 4, Random.Range(-1, 1) * 1, ForceMode.Impulse);
+            //}
+            //foreach (MeshRenderer mesh in renders)
+            //{
+            //    //turn on meshes
+            //    Debug.Log("Turn on Balloon meshes");
+            //    mesh.enabled = true;
+            //}
+            //foreach (ConstantForce force in forces)
+            //{
+            //    //apply constant force
+            //    force.enabled = true;
+            //}
+            time = Time.time;
+            countdown = 0.5f;
             launched = true;
+        }
+        else
+        {
+            launched = false;
         }
         
     }
 
     // Update is called once per frame
     void Update () {
-        totalBallons = balloonBasket1.transform.childCount;
-        Transform[] transforms = balloonBasket1.GetComponentsInChildren<Transform>();
-        print("balloons = " + transforms.Length);
-        foreach (Transform t in transforms)
+        if (launched)
         {
-            if (t.position.y > 200)
-            {
-                Destroy(t.gameObject);
+            
+                totalBalloons = balloonBasket1.transform.childCount;
+                Transform[] transforms = balloonBasket1.GetComponentsInChildren<Transform>();
+                print("balloons = " + transforms.Length);
+                foreach (Transform t in transforms)
+                {
+                    if (t.position.y > 200)
+                    {
+                        Destroy(t.gameObject);
+                    }
+                }
+                if (transforms.Length < 500)
+                {
+                    if (Time.time - time > countdown)
+                    {
+                        GameObject clone = Instantiate(balloonSample, balloonBasket1.transform);
+                        clone.GetComponent<Renderer>().material = balloonColors[Random.Range(0, balloonColors.Length)];
+                        clone.transform.position = balloonBasket1.transform.position;
+                        clone.GetComponent<Rigidbody>().isKinematic = false;
+                    clone.GetComponent<Rigidbody>().AddForce(Random.Range(-1, 1) * 0, 2, Random.Range(-1, 1) * 0, ForceMode.Impulse);
+                    clone.GetComponent<MeshRenderer>().enabled = true;
+                        clone.GetComponent<MeshCollider>().enabled = true;
+                        clone.GetComponent<ConstantForce>().enabled = true;
+                        AudioManager.Instance.PlayBalloonRelease(gameObject);
+                    }
+                }
+                countdown = 0.3f;
             }
-        }
-        if (transforms.Length < 500)
-        {
-            GameObject clone = Instantiate(balloonSample, balloonBasket1.transform);
-            clone.GetComponent<Renderer>().material = balloonColors[Random.Range(0, balloonColors.Length)];
-            clone.transform.position = balloonBasket1.transform.position;
-            clone.GetComponent<Rigidbody>().isKinematic = false;
-            clone.GetComponent<Rigidbody>().AddForce(Random.Range(-1, 1) * 1, 4, Random.Range(-1, 1) * 1, ForceMode.Impulse);
-            clone.GetComponent<MeshRenderer>().enabled = true;
-            clone.GetComponent<ConstantForce>().enabled = true;
-            AudioManager.Instance.PlayBalloonRelease(gameObject);
-        }
     }
 }
