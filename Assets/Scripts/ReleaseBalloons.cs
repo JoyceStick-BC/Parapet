3e9e49bf36edb367bee19ef12fcdeff1b0c21493
rickyyang <rickyyang987@gmail.com>
2019-03-30 21:04:40 -0400

    New stand, texture changes

---
+++ Assets/Scripts/ReleaseBalloons.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReleaseBalloons : MonoBehaviour {
+
+    public GameObject balloonBasket1;
+
+    private GameObject[] balloons;
+
+	// Use this for initialization
+	void Start () {
+		//balloons = balloonBasket1.
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
